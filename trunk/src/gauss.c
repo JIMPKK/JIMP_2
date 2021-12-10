c40d5a91587a0a94b8f2ff7256a681e54d21f534
KacprkLipka <87283828+KacprkLipka@users.noreply.github.com>
2021-12-10 13:25:08 +0100

    Update gauss.c

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -1,53 +1,39 @@
-#include "gauss.h"
+int gauss(Matrix* mat, Matrix* b, Matrix* x){
+  if (mat->c != mat->r || mat->r != b->r) return 1;
 
-/**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
-int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+  int n = x->r; // tworzymy nowy matrix
+  double A[20][20];
+  for (int i = 0; i <= n; i++) {
+    for (int j =0; j <= n; j++) {
+      A[i][j] = mat[i][j];
+    }
+  }
+  for (int i = 0; i <= n; i++){
+    int j = n+1;
+    A[i][j] = b[i][j];
+  }
 
-		return 0;
-}
-/*
-int main()
-{
-  int i, j, k, n;
-  float A[20][20], c, x[10], sum = 0.0;
-  printf("\nEnter the order of matrix: ");
-  scanf("%d", & n);
-  printf("\nEnter the elements of augmented matrix row-wise:\n\n");
-  for (i = 1; i <= n; i++) {
-    for (j = 1; j <= (n + 1); j++) {
-      printf("A[%d][%d] : ", i, j);
-      scanf("%f", & A[i][j]);
-    }
-  }
-  for (j = 1; j <= n; j++) /* loop for the generation of upper triangular matrix {
-    for (i = 1; i <= n; i++) {
-      if (i > j) {
-        c = A[i][j] / A[j][j];
-        for (k = 1; k <= n + 1; k++) {
-          A[i][k] = A[i][k] - c * A[j][k];
-        }
-      }
-    }
-  }
-  x[n] = A[n][n + 1] / A[n][n];
-   this loop is for backward substitution
-  for (i = n - 1; i >= 1; i--) {
-    sum = 0;
-    for (j = i + 1; j <= n; j++) {
-      sum = sum + A[i][j] * x[j];
-    }
-    x[i] = (A[i][n + 1] - sum) / A[i][i];
-  }
-  printf("\nThe solution is: \n");
-  for (i = 1; i <= n; i++) {
-    printf("\nx%d=%f\n", i, x[i]); x1, x2, x3 are the required solutions
-  }
-  return (0);
-}
-*/
+
+  for (j = 0; j <= n; j++) /* pętla, która tworzy macierz trójkątną*/ {
+     for (i = 0; i <= n; i++) {
+       if (i > j) {
+         c = A[i][j] / A[j][j];
+         for (k = 0; k <= n + 1; k++) {
+           A[i][k] = A[i][k] - c * A[j][k];
+         }
+       }
+     }
+   }
+
+   for (int i = 0; i <= n; i++) { // przepisanie z nowego matrixa do starych
+     for (int j = 0; j <= n; j++) {
+       mat[i][j] = A[i][j];
+     }
+   }
+   for (int i = 0; i <= n; i++){
+     int j = n+1;
+     b[i][j] = A[i][j];
+   }
+
+   return 0;
+ }
